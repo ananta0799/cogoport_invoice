9f75098ee98dc358f0d0ebe090eee040f0032193
ananta <ananta@cogoport.com>
2022-10-09 22:07:45 +0530

    change

--- lib/cogoport_invoice.rb
+++ lib/cogoport_invoice.rb
@@ -5,4 +5,13 @@ require_relative "cogoport_invoice/version"
 module CogoportInvoice
   class Error < StandardError; end
   # Your code goes here...
+
+  def create_invoice_combination
+    print "asdfhhh"
+  end
 end
+
+class Invoice
+  include CogoportInvoice
+end
+
