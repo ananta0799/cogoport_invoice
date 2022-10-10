3fc5316352cb0727430d1da63074e1ce286861d6
ananta <ananta@cogoport.com>
2022-10-10 12:56:10 +0530

    change

--- lib/cogoport_invoice.rb
+++ lib/cogoport_invoice.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 require_relative "cogoport_invoice/version"
+require_relative "active_record"
 
 module CogoportInvoice
   class Error < StandardError; end
   # Your code goes here...
 
-  class Invoice
+  class Invoice < ActiveRecord::Base
     def self.create_invoice_combination
       print "asdfhhh"
     end
